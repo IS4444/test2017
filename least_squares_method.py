2be9f7920e48dafad098bfd1b5345add32fa768c
Shota Inoue <0my32347242141b@ezweb.ne.jp>
2018-01-21 22:44:15 +0900

    Update least_squares_method.py

--- least_squares_method.py
+++ least_squares_method.py
@@ -1,11 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 B = 0.0
 D = 0.0
 X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-Y = [200, 186, 164, 160, 148, 137, 134, 122, 113, 98, 89, 77, 71, 62, 49, 38, 30, 21, 12, 3]
+Y = []
 n = 20
+count = 0
+
+print("Mode selection Default:1 Ramdaom:2 Exit:Other keys")
+mode = input('>> ')
+
+if mode == 1:
+   Y = [200.0, 186.0, 164.0, 160.0, 148.0, 137.0, 134.0, 122.0, 113.0, 98.0, 89.0, 77.0, 71.0, 62.0, 49.0, 38.0, 30.0, 21.0, 12.0, 3.0]
+elif mode == 2:
+   while count < n:
+      Y.append(np.random.rand())
+      count += 1
+else:
+   sys.exit()
+
+print(Y)
 
 for i in range(n):
    B += (X[i]*X[i])
@@ -25,6 +41,7 @@ plt.plot(X,Y,'o', label = 'Data')
 
 plt.title('least squares method Graph')
 plt.xlabel('X-Axis')
+plt.text(0,0,"y = %f x + %f"% (a,b),fontsize=18)#2,5
 plt.ylabel('Y-Axis')
 plt.legend()
 
